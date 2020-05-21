c75b907e09b81dc17f3bfb27cf19e04dcfc2b505
NerdyMonarch <npandey@etech-nyc.org>
2020-05-21 00:59:00 -0400

    Game Loop Added

--- src/Game.java
+++ src/Game.java
@@ -42,6 +42,57 @@ public class Game extends Canvas implements Runnable {
 
     // Runs Game
     public void run() {
+        // Obtains Time in NanoSeconds
+        long lastTime = System.nanoTime();
+        // Amount of Ticks per Second in Our Game
+        double ticksPerSecond = 60.0;
+        // Finds Nanoseconds Per Tick
+        double nanoPerTick = 1000000000 / ticksPerSecond;
+        // Holds Difference in Time
+        double delta = 0;
+        // Obtains Time in Milliseconds
+        long timer = System.currentTimeMillis();
+        // Counts Number of Frames
+        int frames = 0;
+        while(running) {
+            // Current Time in NanoSeconds
+            long now = System.nanoTime();
+            // Difference in Time Converted into Ticks
+            delta += (now - lastTime) / nanoPerTick;
+            // Current Time Becomes Previous Time
+            lastTime = now;
+            // Runs Loop Based on Number of Ticks Passed
+            while(delta >= 1) {
+                // Runs this Method for Every Tick Passed
+                tick();
+                // Decrements a Tick because Method was Called
+                delta--;
+            }
+
+            // Updates Window
+            if(running) {
+                render();
+            }
+            // Frame Increases Because Render was Completed
+            frames++;
+            
+            // Checks if a Second Has Passed
+            if(System.currentTimeMillis() - timer > 1000) {
+                // Accounts for the Second that has Passed
+                timer += 1000;
+                // Prints out Frames Rendered in that Second
+                System.out.println("FPS: " + frames);
+                // Resets Frame to 0
+                frames = 0;
+            }
+        }
+    }
+
+    private void tick() {
+
+    }
+
+    private void render() {
 
     }
 
