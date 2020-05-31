318f4e62017d606cb9df0184374a49c54b184022
Jessica <jgarcia@etech-nyc.org>
2020-05-30 20:51:53 -0400

    Rewritten Abstract and Enum Implementation

--- src/com/teamW/tiles/Tile.java
+++ src/com/teamW/tiles/Tile.java
@@ -1,5 +1,13 @@
 package com.teamW.tiles;
+import java.awt.image.BufferedImage;
 
 public abstract class Tile {
-    
+    protected BufferedImage[] images;
+    protected TileType TileType;
+
+    public Tile(BufferedImage[] images, TileType TileType) {
+        this.images = images;
+        this.TileType = TileType;
+
+    }
 }
